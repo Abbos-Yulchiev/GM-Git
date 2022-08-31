980a72b91e0347556f605cde7a8d82425f843819
Abbos Yulchiev <Abbos_Yulchiev@epam.com>
2022-08-31 09:57:41 +0500

    4 parametirized add method added

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -3,9 +3,16 @@ public class Add {
     public int add(int a, int b) {
         System.out.println("This is add method for 2 params");
         return a + b;
-    }public int add(int a, int b, int c) {
+    }
+
+    public int add(int a, int b, int c) {
         System.out.println("This is add method for 3 params");
         return a + b + c;
+    }
+
+    public int add(int a, int b, int c, int d) {
+        System.out.println("This is add method for 4 params");
+        return a + b + c + d;
     }
 
     public int subtraction(int a, int b) {
